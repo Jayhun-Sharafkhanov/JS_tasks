b3b56ee385223d1cc93f6e623a2fa449b711f040
Jayhun <jayhunshrfkhan19@gmail.com>
2022-08-19 14:14:47 +0400

    km to mil

---
+++ task3/app.js
@@ -0,0 +1,8 @@
+//3.km to mil
+//Kilometr deyerini mil'e cevirin,  1km 0,6214 mile beraberdir.
+//User'den kilometr deyerini alin ve alert ile mil deyerini gosterin.
+
+
+let km = prompt('kilometr daxil edin: ')
+let mil = km * 0.621314
+alert(mil)
