d19770fc775a7d9400cb229071412ff617e5eeaf
Jayhun <jayhunshrfkhan19@gmail.com>
2022-08-19 14:03:21 +0400

    Area of rectangle

---
+++ task1/app.js
@@ -0,0 +1,15 @@
+//1. area_of_rectangle
+//
+//User will enter the sides of rectangle: width and length.
+// Create variables and assign these to variables.
+//  Later, calculate the area of the rectangle and alert it 
+//
+//Istifadeci duzbucagin tereflerini daxil edecek: en ve uzunluq.
+// Deyisenler yaradin ve bunlari hemin deyisenlere menimsedin.
+// Daha sonra saheni hesablayin ve istifadeciye gosterin.
+
+
+let en = prompt('Duzbucaqlinin enini daxil edin: ')
+let uzunluq = prompt('Duzbucaqlinin uzunlugunu daxil edin:')
+let sahe = en * uzunluq
+alert('Duzbucaqlinin sahesi:' + sahe)
