97ff410fe03f2b494dbe2ac0c1a7017eafc83238
Jayhun <jayhunshrfkhan19@gmail.com>
2022-08-19 14:09:19 +0400

    age_calculator

---
+++ task2/app.js
@@ -0,0 +1,10 @@
+//2. age_calculator
+//User will enter his/her birth date. 
+//Find his/her age and alert it. 
+//
+//Istifadeci oz dogum ilini daxil edecek.
+// Onun yasini tap ve goster.
+
+
+let tevellud = prompt('Tevelludunuzu daxil edin:')
+alert(2022 - tevellud)
