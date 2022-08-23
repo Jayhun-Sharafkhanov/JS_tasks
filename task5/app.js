b3dae33385f4864a9996bf00e16235fdacb4b629
Jayhun <jayhunshrfkhan19@gmail.com>
2022-08-23 17:00:29 +0400

    basic calculator

---
+++ task5/app.js
@@ -0,0 +1,16 @@
+let eded1 = Number(prompt('ededi daxil edin:'))
+let operator = prompt('operator daxil edin:')
+let eded2 = Number(prompt('ededi daxil edin:'))
+
+    if( operator=== '+' ){
+    console.log( eded1 + eded2 )
+   }
+    else if(operator=== '-'){
+        console.log(eded1 - eded2)
+    }
+    else if(operator=== '*'){
+        console.log(eded1 * eded2)
+    }
+    else if(operator=== '/'){
+        console.log(eded1 / eded2)
+    }
