493420ed7495a68a4d38a43c617058b6e2f52ced
Jayhun <jayhunshrfkhan19@gmail.com>
2022-08-26 17:16:18 +0400

    stars

---
+++ task4/app.js
@@ -0,0 +1,26 @@
+for(i=1; i<=1; i++){
+    
+    document.write('*')
+}
+document.write('<br/>')
+for(i=1; i<=4; i++){
+    
+    document.write('*')
+}
+document.write('<br/>')
+for(i=1; i<=3; i++){
+    
+    document.write('*')
+}
+document.write('<br/>')
+for(i=1; i<=2; i++){
+    
+    document.write('*')
+}
+document.write('<br/>')
+for(i=1; i<=1; i++){
+    
+    document.write('*')
+}
+document.write('<br/>')
+
