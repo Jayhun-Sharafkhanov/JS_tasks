ebe88ddcc82fc39b7fab223c1d942c7a4e1b37dd
Jayhun <jayhunshrfkhan19@gmail.com>
2022-08-27 14:54:51 +0400

    function ededquvveti

---
+++ task7/app.js
@@ -0,0 +1,12 @@
+function getNumber(){
+  return Number(prompt('enter the number:'))
+}
+
+function ededinQuvveti (n1 , n2){
+  return (n1 + n2)**2
+}
+
+let n1 = getNumber()
+let n2 = getNumber()
+
+console.log(ededinQuvveti(n1 , n2))
